171ddcf3b8879a324d2ba581b2207dd529700b66
Md. Al Mamun <mdalmamun5354@gmail.com>
2024-04-19 10:48:02 +0600

    Done!  read data, login and logout compleate

--- Fronend/src/App.jsx
+++ Fronend/src/App.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import Home from './home/Home'
-import Courses from './courses/Courses';
-import Contact from './contact/Contact';
-import { Route, Routes } from 'react-router-dom';
-import Signup from './components/Signup';
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-      {/* <Home />
-      <Course /> */}
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/course' element={<Courses />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Fronend/src/components/Course.jsx
+++ Fronend/src/components/Course.jsx
@@ -1,12 +1,25 @@
-import Card from "./Card";
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import bookList from '../../public/list.json'
-
 function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="pt-28 items-center justify-center text-center">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
           <h1 className="text-2xl  md:text-4xl">
             We're delighted to have you{" "}
             <span className="text-pink-500"> Here! :)</span>
@@ -20,20 +33,20 @@ function Course() {
             animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
             consequatur!
           </p>
-          <Link to='/'>
+          <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
               Back
             </button>
           </Link>
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
-          {bookList.map((book) => (
-            <Card key={book._id} book={book} />
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
           ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Course;
